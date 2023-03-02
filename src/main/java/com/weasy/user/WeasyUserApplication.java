a6d55e2b483cb8d3b11cf200227e0406e72fbf30
akdsksdyd <kakalote4@gmail.com>
2023-03-02 14:33:34 +0900

    0302 14:33 first commit

---
+++ src/main/java/com/weasy/user/WeasyUserApplication.java
@@ -0,0 +1,13 @@
+package com.weasy.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeasyUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeasyUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weasy/user/WeasyUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weasy.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeasyUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
